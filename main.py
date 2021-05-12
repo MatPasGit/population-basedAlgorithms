55767b64d8b4517e7f0f2d873de7431282fc45a6
MatPasGit <47747551+MatPasGit@users.noreply.github.com>
2021-05-12 12:30:21 +0200

    clear main

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from InstanceGenerator import instanceGenerator
+from Route import Route
 
 
 def main():
-    c = instanceGenerator(20, 5)
-    k = c[0][1]
-    print(k)
-    print("siema")
-    list=[0,1,2,3,4]
+    problem_instance = instanceGenerator(20, 5)
+    r = Route([0,1,2,3,4], problem_instance)
+    print(r.get_value())
+    print(r.get_route_list())
 
 
 main()
