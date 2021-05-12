2a39f2398ef40fd97db94226307ec761dc8ecedf
MatPasGit <47747551+MatPasGit@users.noreply.github.com>
2021-05-12 12:10:49 +0200

    problem init works

--- InstanceGenerator.py
+++ InstanceGenerator.py
@@ -4,14 +4,11 @@ from RandomNumberGenerator import RandomNumberGenerator
 def instanceGenerator(Z, size):
     generator = RandomNumberGenerator(Z)
     d = [[0 for i in range(size)] for j in range(size)]
-    w = [[0 for i in range(size)] for j in range(size)]
 
     for i in range(size):
         for j in range(size):
             if (i > j):
                 d[i][j] = generator.nextInt(1, 50)
-                d[j][i] = d[i][j]
-                w[i][j] = generator.nextInt(1, 50)
-                w[j][i] = w[i][j]
+                d[j][i] = generator.nextInt(1, 50)
 
-    return w, d
+    return d
--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from InstanceGenerator import instanceGenerator
 
 
 def main():
-    print("LOL")
     c = instanceGenerator(20, 5)
     k = c[0][1]
-    print(c)
+    print(k)
     print("siema")
 
 main()
