cb2b6a482764d8e8d380c7f499ebc2d4994a5667
MatPasGit <47747551+MatPasGit@users.noreply.github.com>
2021-05-13 18:04:21 +0200

    gatherers added

---
+++ cross.py
@@ -0,0 +1,41 @@
+import random
+
+from Route import Route
+
+
+class Cross:
+    _parent_one = []
+    _parent_sec = []
+    _cross_points = 2
+    PROBLEM_MATRIX = []
+
+    def __init__(self, parent1, parent2, cross_points, problem_matrix):
+        self._parent_one = parent1
+        self._parent_sec = parent2
+        self._cross_points = cross_points
+        self.PROBLEM_MATRIX = problem_matrix
+
+    def OX_cross(self):
+        allels = self._cross_points + 1
+        allels = int( len(self._parent_one)/allels) +1
+        print("ALLELS : ", allels)
+
+        child=[]
+
+        luck = random.randint(0, 1)
+        print(luck)             #losowanie chromosomu
+        if luck == 0:
+            for i in range(0, allels):
+                child.append(self._parent_one[i])
+        if luck == 1:
+            for i in range(allels, allels+allels):
+                child.append(self._parent_one[i])
+
+        for i in range(0, len(self._parent_sec)):
+            if self._parent_sec[i] in child :
+                continue
+            else:
+                child.append(self._parent_sec[i])
+
+        return Route(child, self.PROBLEM_MATRIX)
+
--- main.py
+++ main.py
@@ -1,12 +1,71 @@
+import copy
+
 from InstanceGenerator import instanceGenerator
 from Route import Route
+import random
+import operator
+from cross import Cross
 
 
 def main():
-    problem_instance = instanceGenerator(20, 5)
-    r = Route([0,1,2,3,4], problem_instance)
-    print(r.get_value())
-    print(r.get_route_list())
+    MAX_CHANGE_LIMIT = 10
+    ITERATIONS = 1000
+    PROBLEM_SIZE = 8
+    SEED = 20
+    PROBLEM_MATRIX = instanceGenerator(SEED, PROBLEM_SIZE)
+    CROSS_POINTS = 2
+
+    #INIT SOLUTION
+    init_list = []
+    for i in range(0, PROBLEM_SIZE):
+        init_list.append(i)
+
+    BEES_LIST=[]
+    BEST_SOLUTION = Route(init_list, PROBLEM_MATRIX)
+
+    # INITIALIZING BEES
+    for i in range(0, PROBLEM_SIZE):
+        random.shuffle(init_list)
+        x = Route( copy.deepcopy(init_list), PROBLEM_MATRIX)
+        if i == 0:
+            BEST_SOLUTION = copy.deepcopy(x)
+        if x.get_value() > BEST_SOLUTION.get_value():
+            BEST_SOLUTION =  copy.deepcopy(x)
+        BEES_LIST.append(x)
+
+
+    BEES_LIST.sort(key=operator.attrgetter('_value'))
+
+    loop = 0
+    while loop > ITERATIONS:
+        loop += 1
+
+
+    #ZBIERACZE
+    for i in range(0, len(BEES_LIST)):
+        child = None
+
+        luck = random.randint(0, 1)
+        if luck == 0:
+            if i ==  len(BEES_LIST):
+                child = Cross(BEES_LIST[i].get_route_list(), BEES_LIST[0].get_route_list(), CROSS_POINTS, PROBLEM_MATRIX)
+            else:
+                child = Cross(BEES_LIST[i].get_route_list(), BEES_LIST[i+1].get_route_list(), CROSS_POINTS,PROBLEM_MATRIX )
+        if luck == 1:
+            if i ==  0:
+                child = Cross(BEES_LIST[i].get_route_list(), BEES_LIST[len(BEES_LIST) - 1].get_route_list(), CROSS_POINTS, PROBLEM_MATRIX)
+            else:
+                child = Cross(BEES_LIST[i].get_route_list(), BEES_LIST[i-1].get_route_list(), CROSS_POINTS,PROBLEM_MATRIX )
+        new_solution = child.OX_cross()
+
+        if(new_solution.get_value() > BEES_LIST[i].get_value() ):
+            BEES_LIST[i] = copy.deepcopy(new_solution)
+        else:
+            BEES_LIST[i].increment_count()
+            if BEES_LIST[i].get_count() > MAX_CHANGE_LIMIT:     ##ABOVE MAX CHANGE COUNT (Nowy osobnik do poplacji)
+                random.shuffle(init_list)
+                x = Route(copy.deepcopy(init_list), PROBLEM_MATRIX)
+
 
 
 main()
